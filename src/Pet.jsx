6dc79e0615777ecd4b27b59e427cc0bbd8c0acb1
duongnq2798 <duongnq27.98@gmail.com>
2020-11-02 12:58:32 +0700

    Hook Done

---
+++ src/Pet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Pet({ name, animal, breed }) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  )
+}
+export default Pet;
